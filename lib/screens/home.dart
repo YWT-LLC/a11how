e8a3f88cb2654e8707b56fca1b45b9c595715b9b
empathictech <105748722+empathictech@users.noreply.github.com>
2026-09-12 14:46:41 -0400

    trimmed

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,17 +3,14 @@
  * See LICENSE for distribution and usage details.
  */
 
-import '../screens/export.dart';
 import '../utils/export.dart';
 import '../widgets/export.dart';
 
 import 'dart:io';
-import 'dart:math';
 import 'dart:convert';
 import 'package:open_ui/open_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:go_router/go_router.dart';
 import 'package:file_picker/file_picker.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -42,17 +39,6 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) => Consumer<EzCP>(
         builder: (_, EzCP config, __) {
-          double colWidth = max(widthOf(context) / 4, ScreenSize.small.size);
-
-          final BoxDecoration colDeco = BoxDecoration(
-            border: Border.all(
-              color: config.colors.onSurface,
-              width: config.borderWidth,
-            ),
-            borderRadius: BorderRadius.zero,
-            color: config.colors.surface,
-          );
-
           return A11howScaffold(
             config,
             body: EzScreen(
@@ -109,73 +95,11 @@ class _HomeScreenState extends State<HomeScreen> {
                               : selectedDirectory;
                         }).whenComplete(() => setState(() {})),
                       ))
-                    : ReorderableListView(
-                        buildDefaultDragHandles: false,
-                        scrollDirection: Axis.horizontal,
-                        onReorderItem: (int oldIndex, int newIndex) {
-                          if (oldIndex == newIndex) return;
-                          if (oldIndex < newIndex) newIndex -= 1;
-
-                          final ARBFile item = arbFiles.removeAt(oldIndex);
-                          arbFiles.insert(newIndex, item);
-
-                          setState(() {});
-                        },
-                        // header: TODO,
-                        children: arbFiles.map((ARBFile arb) {
-                          // Filter metadata keys (starts with '@')
-                          final List<String> keys =
-                              arb.entries.keys.where((String k) => !k.startsWith('@')).toList();
-
-                          return Container(
-                            key: ValueKey<String>(arb.path),
-                            width: colWidth,
-                            decoration: colDeco,
-                            child: EzCol(mainAxisSize: MainAxisSize.max, children: <Widget>[
-                              // Header
-                              Container(
-                                width: double.infinity,
-                                color: config.colors.secondary,
-                                child: Text(
-                                  '${arb.localeCode} (HUMAN_VER)',
-                                  style:
-                                      config.titleStyle?.copyWith(color: config.colors.onSecondary),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ),
-
-                              // Entries
-                              Expanded(
-                                  child: ListView.builder(
-                                itemCount: keys.length,
-                                itemBuilder: (_, int index) {
-                                  final String key = keys[index];
-                                  final String value = arb.entries[key];
-
-                                  return EzTextField(
-                                    constraints: BoxConstraints(maxWidth: colWidth),
-                                    controller: TextEditingController(text: value.toString()),
-                                    hintText: key,
-                                    maxLines: null,
-                                    textAlign: TextAlign.start,
-                                    validator: (_) => null,
-                                  );
-                                },
-                              )),
-                            ]),
-                          );
-                        }).toList(),
-                      ),
+                    : const EzCol(),
               ),
             ),
             isHome: true,
-            fabs: <Widget>[
-              FloatingActionButton(
-                onPressed: () => context.goNamed(settingsHubPath),
-                tooltip: config.ezL10n.ssNavHint,
-                child: EzIcon(config, Icons.settings),
-              ),
-            ],
+            fabs: <Widget>[SettingsFAB(config, context: context)],
           );
         },
       );
