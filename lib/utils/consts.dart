91171e2fb6460f8ded2ad09f8126a094e9a068f3
empathictech <105748722+empathictech@users.noreply.github.com>
2026-09-14 07:27:53 -0400

    adding local prefs

--- lib/utils/consts.dart
+++ lib/utils/consts.dart
@@ -3,11 +3,30 @@
  * See LICENSE for distribution and usage details.
  */
 
+import 'package:open_ui/open_ui.dart';
+
+//* App config *//
+
 /// a11How
 const String appName = 'a11How';
 
 /// llc.ywt.a11how
 const String androidPackage = 'llc.ywt.a11how';
+
+const String recentProjectsKey = 'recentProjects';
+
+final Map<String, Object> a11HowMobile = <String, Object>{
+  ...ywtMobileConfig,
+  recentProjectsKey: <String>[],
+};
+final Map<String, Object> a11HowDesktop = <String, Object>{
+  ...ywtDesktopConfig,
+  darkShowBackFABKey: true,
+  lightShowBackFABKey: true,
+  recentProjectsKey: <String>[],
+};
+
+//* Models *//
 
 class ARBFile {
   final String path;
