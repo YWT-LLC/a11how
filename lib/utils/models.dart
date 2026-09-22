3ead108472276ad309a3807dbc5f076f9df5095c
empathictech <105748722+empathictech@users.noreply.github.com>
2026-09-22 08:25:53 -0400

    reverted

--- lib/utils/models.dart
+++ lib/utils/models.dart
@@ -25,7 +25,7 @@ class ARBFile {
   final String path;
   final bool local;
   final String localeCode;
-  final Map<String, String> entries;
+  final Map<String, dynamic> entries;
 
   ARBFile({
     required this.path,
